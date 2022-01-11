a65ad8db5fa89cf4e12ef1cdbdc819a909188510
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-11 20:25:02 +0300

    7.4 generics constraints

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ promise.then((value) => {
 
 // generic function
 
-const merge = <T, U>(objA: T, objB: U) => {
+const merge = <T extends object, U extends object>(objA: T, objB: U) => {
 	return Object.assign(objA, objB);
 };
 
