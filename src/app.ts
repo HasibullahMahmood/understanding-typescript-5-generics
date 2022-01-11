24a0352f68e76296f8a94aae7bd72519b368c2db
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-11 20:35:24 +0300

    7.5 generic function another example

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,23 @@ promise.then((value) => {
 });
 
 // generic function
-
 const merge = <T extends object, U extends object>(objA: T, objB: U) => {
 	return Object.assign(objA, objB);
 };
 
 const mergedObj = merge({ name: 'Hasibullah' }, { age: 25 });
 console.log(mergedObj.name);
+
+// another generic function example
+type Lengthy = { length: number };
+const countAndDescribe = <T extends Lengthy>(element: T) => {
+	let description = 'Got no value.';
+	if (element.length > 0) {
+		description = `Got ${element.length} value`;
+	}
+	return [element, description];
+};
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe([1, 2]));
+console.log(countAndDescribe(''));
