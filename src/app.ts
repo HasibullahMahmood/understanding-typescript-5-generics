d028908383dfb0572556cdec93f9acf5c1208288
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-01-11 20:13:09 +0300

    7.3 generic function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+const names: Array<string> = [];
+// names[0].split('');
+
+const promise: Promise<number> = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(1000);
+	}, 2000);
+});
+
+promise.then((value) => {
+	value.toFixed(2);
+});
+
+// generic function
+
+const merge = <T, U>(objA: T, objB: U) => {
+	return Object.assign(objA, objB);
+};
+
+const mergedObj = merge({ name: 'Hasibullah' }, { age: 25 });
+console.log(mergedObj.name);
